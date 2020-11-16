4d7b7cda107aa47ecac59e2e66f4279d34305bf4
Marques Alexandre <64232193+Marques-Alexandre@users.noreply.github.com>
2020-11-16 19:13:52 -0300

    Tabelas cad_fornecedor e cad_categoria

--- Projeto_Make&Cia/bdlsi.sql
+++ Projeto_Make&Cia/bdlsi.sql
@@ -1,8 +1,12 @@
 -- MySQL dump 10.13  Distrib 5.7.12, for Win64 (x86_64)
 
--- Tabela Cadastro de produto
+USE bdlsi;
+
+
+-- Tabela cad_produto
 
 DROP TABLE IF EXISTS `cad_produto`;
+
 CREATE TABLE `cad_produto` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `codigo` varchar(45) DEFAULT NULL,
@@ -13,10 +17,12 @@ CREATE TABLE `cad_produto` (
   `cod_fornecedor` int(11) DEFAULT NULL,
   `quantidade` int(11) DEFAULT '0',
   PRIMARY KEY (`id`),
-  UNIQUE KEY `codigo_UNIQUE` (`codigo`)
-)ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+  UNIQUE KEY `codigo_UNIQUE` (`codigo`),
+  FOREIGN KEY (categoria) REFERENCES cad_categoria (id),
+  FOREIGN KEY (cod_fornecedor) REFERENCES cad_fornecedor (id)
+)ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=UTF8;
 
--- Tabela Cadastro de usuario
+-- Tabela cad_usuario
 
 create table cad_usuario(
     id int not null auto_increment,
@@ -27,7 +33,7 @@ create table cad_usuario(
     PRIMARY KEY(id)
 )ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
 
--- Tabela Cadastro de vendas
+-- Tabela vendasProduto
 
 create table vendasProduto(
     id_VendasProdutos int not null auto_increment,
@@ -35,15 +41,36 @@ create table vendasProduto(
     produto int not null,
     preço double not null,
     PRIMARY KEY(id_VendasProdutos),
-    FOREIGN KEY (pedido) REFERENCES vendas (id_vendas)
+    FOREIGN KEY (pedido) REFERENCES vendas (id_venda)
 )ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
 
+-- Tabela vendas
 
 create table vendas(
-    id_vendas int not null auto_increment,
+    id_venda int not null auto_increment,
     data date,
-    tipo varchar(20),
-    PRIMARY KEY(id_vendas)
-)ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+    forma_pagamento varchar(20),
+    PRIMARY KEY(id_venda)
+)ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=UTF8;
 
+-- Tabela cad_fornecedor
+
+CREATE TABLE cad_fornecedor(
+	id INT NOT NULL AUTO_INCREMENT,
+	nome VARCHAR(50) NOT NULL,
+	cnpj VARCHAR(15) NOT NULL,
+	endereco VARCHAR(80) NOT NULL,
+	cidade VARCHAR(40) NOT NULL,
+	telefone VARCHAR(20) NOT NULL,
+	PRIMARY KEY(id)
+)ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=UTF8;
+
+-- Tabela cad_categoria
+
+CREATE TABLE cad_categoria(
+	id INT NOT NULL AUTO_INCREMENT,
+	nome VARCHAR(50) NOT NULL,
+	descricao VARCHAR(70) NOT NULL,
+	PRIMARY KEY(id)
+)ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=UTF8;
 
